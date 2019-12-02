e8fccd38ce42898d9e4b537bb52ca3defd58c664
K Dll <k.dally@student.tudelft.nl>
2019-12-03 00:57:26 +0100

    Q7

--- F16Sim/FindF16Dynamics.m
+++ F16Sim/FindF16Dynamics.m
@@ -185,7 +185,7 @@ for i=1:length( D_longitude_hi(:,1) )
     mprintf([ D_longitude_hi(i,:) ],'  %.3e ')
 end %for
 
-rifd(long_poles_hi)
+rifd(long_poles_hi);
 
 disp(newline);
 
@@ -213,7 +213,7 @@ for i=1:length( D_lateral_hi(:,1) )
     mprintf([ D_lateral_hi(i,:) ],'  %.3e ')
 end %for
 
-rifd(lat_poles_hi)
+rifd(lat_poles_hi);
 
 disp(newline);
 disp(newline);
@@ -242,7 +242,7 @@ for i=1:length( D_longitude_lo(:,1) )
 end %for
 
 % Display the real, imaginary, frequency (magnitude) and damping ratios
-rifd(long_poles_lo)
+rifd(long_poles_lo);
 
 disp(newline);
 
@@ -271,7 +271,7 @@ for i=1:length( D_lateral_lo(:,1) )
 end %for
 
 % Display the real, imaginary, frequency (magnitude) and damping ratios
-rifd(lat_poles_lo)
+rifd(lat_poles_lo);
 
 %% All Poles
 figure(1); 
@@ -457,20 +457,21 @@ y=step(SS_shortp,t);
 % legend("aoa","pitch rate");
 % grid on
 
-H_q_de = tf(SS_shortp(2,1));
+H_shortp = tf(SS_shortp);
+H_q_de = H_shortp(2);
 [num_q_de,den_q_de] = tfdata(SS_shortp(2,1));
 num_q_de = cell2mat(num_q_de);
 den_q_de = cell2mat(den_q_de);
 
-H_alpha_de = tf(SS_shortp(1,1));
+H_aa_de = tf(SS_shortp(1,1));
 
 % open loop current properties
 k_q = num_q_de(3); %Assumed to be constant
-T_theta2 = num_q_de(2)/k_q;
+T_tt2 = num_q_de(2)/k_q;
 freq_shortp = (den_q_de(3))^0.5;
 damp_shortp = den_q_de(2) / (2 * freq_shortp);
 g = 9.80665;
-CAP = g * freq_shortp^2 * T_theta2 / (velocity * 0.3048);
+CAP = g * freq_shortp^2 * T_tt2 / (velocity * 0.3048);
 
 
 % required parameters (rq)
@@ -478,6 +479,7 @@ CAP_rq = g * 0.03 / 0.75;
 damp_shortp_rq = 0.5;
 freq_shortp_rq = 0.03 * velocity * 0.3048;
 T_tt2_rq = CAP_rq * velocity *0.3048 /(g*freq_shortp_rq^2); 
+DB_qs_rq = T_tt2_rq - 2 * damp_shortp_rq / freq_shortp_rq;
 
 %using Ackermann's formula
 s=tf('s');
@@ -490,14 +492,98 @@ K_ackermann = [0 1] * C_m^(-1)*p_alfa;
 A_ackermann = A_shortp-B_shortp*K_ackermann;
 sys_shortp_cl = ss(A_ackermann, B_shortp,C_shortp,D_shortp);
 
-damp(sys_shortp_cl);
-;
+%damp(sys_shortp_cl);
+
 lead_lag=(k_q*(1+T_tt2_rq * s))/(num_q_de(3)+num_q_de(2)*s);
 H_q_de_cl = minreal(tf(sys_shortp_cl(2)) * lead_lag);
 H_aa_de_cl = minreal(tf(sys_shortp_cl(1)) * lead_lag);
 
+H_shortp_cl = [H_q_de_cl; H_q_de_cl/s];
 
+%% Close Loop Performance Evaluation
+[num_q_de_cl,den_q_de_cl] = tfdata(H_q_de_cl);
 
+num_q_de_cl = cell2mat(num_q_de_cl);
+den_q_de_cl = cell2mat(den_q_de_cl);
+freq_shortp_cl = (den_q_de_cl(3))^0.5;
+damp_shortp_cl = den_q_de_cl(2) / (2 * freq_shortp_cl);
+T_tt2_cl = num_q_de_cl(2)/k_q;
+CAP_cl = g * freq_shortp_cl^2 * T_tt2_cl / (velocity * 0.3048);
+
+%% TIME RESPONSE
+dt = 0.01;
+t = [0:dt:30];
+u_step_10s = [ones(1,10/dt+1) zeros(1,(30-10)/dt)];
+
+y=lsim(-H_shortp_cl,u_step_10s,t);  %Explain why negative
+
+figure(7);
+plot(t,y,'linewidth',2) 
+hold on
+plot(t,u_step_10s,'linewidth',2) 
 %gamma_0 = y_long(1,3) - y_long(1,2)
-y=step(H_aa_de_cl,t);
-plot(t,y)
+legend('pitch rate','pitch attitude','input','location','northwest')
+grid on
+grid minor
+xlabel('time [s]');
+ylabel('[\circ] and [\circ/s]');
+title('Input and Short period response','FontWeight','Normal')
+set(gca,'FontSize',15)
+
+%% PLOT CAP
+figure(8);
+patch([0.3 0.3 1.5 1.5 0.3],[0.28 3.6 3.6 0.28 0.28],[0.9100    0.200    0.1700],'FaceAlpha',0.2)
+hold on
+patch([0.25 0.25 2 2 0.25],[0.16 10 10 0.16 0.16],[0.9290, 0.640, 0.1250],'FaceAlpha',0.2)
+patch([0.125 0.125 10 10 0.125],[0.01 10 10 0.01 0.01],[1,1,0.3],'FaceAlpha',0.2)
+%xline(0.125,'linewidth',2);
+plot(damp_shortp_cl,CAP_cl,'r*','linewidth',2) 
+ylim([0.01 10])
+xlim([0.1 10])
+grid on
+xlabel('\zeta_{sp} [-]');
+ylabel('CAP [1/gs^2]');
+legend('level 1 cat. A','level 2 cat. A','level 3 cat. A','current parameter value','Location','Southeast')
+title('Control Anticipation Parameter versus \zeta_{sp}','FontWeight','Normal')
+set(gca, 'XScale', 'log', 'YScale','log','FontSize',15)
+
+
+%% GIBSON DROPBACK CRITERION
+qm_qs = max(y(:,2))/y(end,2);
+DB_qs = (y(t==10,2)-y(end,2))/y(end,2);
+figure(10);
+patch([0 0 0.06 0.3], [1 3 3 1],[0, 0.4470, 0.7410],'FaceAlpha',0.3);
+hold on 
+plot(DB_qs,qm_qs,'r*','linewidth',2)  
+plot(DB_qs_rq,qm_qs,'k*','linewidth',2)
+ylim([1 4])
+xlim([-0.4 0.6])
+title({'Gibson Dropback Criterion'},'FontWeight','Normal')
+grid on
+xlabel('(DB/q_s) [s]');
+ylabel('q_m/q_s [-]');
+legend('satisfactory region','current parameter value','design parameter value')
+set(gca,'FontSize',15)
+
+
+%% GIBSON RATE CRITERION
+[G_Wcp,P_Wcp,Wcg,Wcp] = margin(H_q_de*lead_lag); %Change TF to cl
+[G_2Wcp,P_2Wcp] = bode(H_q_de*lead_lag,2*Wcp);
+phase_rate = -(P_2Wcp-P_Wcp)/(Wcp);
+figure(9);
+l = linspace(0, 2*pi, 200);
+x = 0.3 * cos(l) + 1.2;
+y = 20 * sin(l) + 75;
+patch(x, y, [0, 0.4470, 0.7410],'FaceAlpha',0.3);
+hold on 
+plot(DB_q_ss_cl,2.5,'r*','linewidth',2)  %check qm/qs
+yline(100,'--k');
+ylim([0 300])
+xlim([0 2])
+title({'Gibson Rate Criterion'},'FontWeight','Normal')
+grid on
+xlabel('-180^{\circ} phase lag frequency [Hz]');
+ylabel('phase rate [^{\circ}/Hz]');
+legend('optimal region','current parameter value')
+set(gca,'FontSize',15);
+
